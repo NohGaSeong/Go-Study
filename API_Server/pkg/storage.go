18de06ae759dcf51ed0a26ead8e370a80c7f9901
yohan <noyohanx@naver.com>
2023-07-21 06:18:41 +0900

    :sparkles: :: storage 기본 세팅

---
+++ API_Server/pkg/storage.go
@@ -0,0 +1,58 @@
+package pkg
+
+import (
+	"fmt"
+	"sync"
+)
+
+type EntityRepository interface {
+	Init()
+	Add(entity *Entity) error
+	Update(entity *Entity) error
+	Get(id string) (*Entity, error)
+	List(page, pageSize int) ([]*Entity, error)
+	Delete(id string) error
+}
+
+type EntityMemoryRepository struct {
+	mutex    sync.RWMutex
+	entities []*Entity
+}
+
+func NewEntityMemoryRepository() *EntityMemoryRepository {
+	return &EntityMemoryRepository{
+		entities: []*Entity{},
+	}
+}
+
+func (r *EntityMemoryRepository) Init() {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	r.entities = []*Entity{
+		{
+			ID:		"1",
+			Type:	PersonEntityType,
+			Name:	"John",
+			Description: "John is a person",
+		},
+		{
+			ID:		"2",
+			Type:	CompanyEntityType,
+			Name:	"Google",
+			Description: "Google is a company",
+		},
+		{
+			ID:		"3",
+			Type:	PlaceEntityType,
+			Name:	"seoul",
+			Description: "seoul is a place",
+		},
+		{
+			ID:		"4",
+			Type:	BookEntityType,
+			Name:	"The Little Prince",
+			Description: "The Little Prince is a book",
+		},
+	}
+}

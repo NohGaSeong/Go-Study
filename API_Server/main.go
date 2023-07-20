bc5a29e66ff24d5c8a37b00a914b2e33903daab8
yohan <noyohanx@naver.com>
2023-07-20 10:57:25 +0900

    :sparkles: :: Response Json

--- API_Server/main.go
+++ API_Server/main.go
@@ -6,11 +6,17 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type JSONTextResponse struct {
+	Message string
+}
+
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello Go Fiber!")
+		// return c.SendString("Hello Go Fiber!")
+
+		return c.JSON(JSONTextResponse{Message: "Hello Fiber"})
 	})
 
 	log.Fatal(app.Listen(":3000"))

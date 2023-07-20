98a88e67f290c9bc816fb67eed8f867e93cdd478
yohan <noyohanx@naver.com>
2023-07-21 06:03:04 +0900

    :sparkles: :: create model

---
+++ API_Server/pkg/model.go
@@ -0,0 +1,30 @@
+package pkg
+
+type (
+	TextResponse struct {
+		Message string `json:"message"`
+	}
+
+	Entity struct {
+		ID          string     `json:"id"`
+		Type        EntityType `json:"type" form:"type"`
+		Name        string     `json:"name" form:"name"`
+		Description string     `json:"description" form:"description"`
+	}
+
+	EntityType string
+)
+
+
+const (
+	UknownEntityType	EntityType = "unknown"
+	PersonEntityType	EntityType = "person"
+	CompanyEntityType	EntityType = "company"
+	PlaceEntityType		EntityType = "place"
+	BookEntityType		EntityType = "book"
+	MovieEntityType		EntityType = "movie"
+	TvSeriesEntityType	EntityType = "tv_series"
+	GameEntityType		EntityType = "game"
+	AlbumEntityType		EntityType = "album"
+	SongEntityType		EntityType = "song"
+)

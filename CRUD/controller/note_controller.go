52c4a2060e71bb1d33ef762a8b4e7dd5df040790
yohan <noyohanx@naver.com>
2023-07-24 20:17:44 +0900

    :sparkles: :: update 기능 추가

--- CRUD/controller/note_controller.go
+++ CRUD/controller/note_controller.go
@@ -5,6 +5,7 @@ import (
 	"golang-fiber-crud/data/response"
 	"golang-fiber-crud/helper"
 	"golang-fiber-crud/service"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -23,6 +24,29 @@ func (controller *NoteController) Create(ctx *fiber.Ctx) error {
 	helper.ErrorPanic(err)
 
 	controller.noteService.Create(createNoteRequest)
+
+	webResponse := response.Response{
+		Code:    200,
+		Status:  "OK",
+		Message: "success",
+		Data:    nil,
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(webResponse)
+}
+
+func (controller *NoteController) Update(ctx *fiber.Ctx) error {
+	updateNoteRequest := request.UpdateNoteRequest{}
+	err := ctx.BodyParser(&updateNoteRequest)
+	helper.ErrorPanic(err)
+
+	noteId := ctx.Params("noteId")
+	id, err := strconv.Atoi(noteId)
+	helper.ErrorPanic(err)
+
+	updateNoteRequest.Id = id
+
+	controller.noteService.Update(updateNoteRequest)
 
 	webResponse := response.Response{
 		Code:    200,

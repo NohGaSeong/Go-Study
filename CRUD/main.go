e27984d4e633e814fa73e9a3df827d757e1bf7f8
yohan <noyohanx@naver.com>
2023-07-24 20:34:59 +0900

    :sparkles: :: 다른 파트들 연결

--- CRUD/main.go
+++ CRUD/main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"golang-fiber-crud/config"
+	"golang-fiber-crud/controller"
+	"golang-fiber-crud/model"
+	"golang-fiber-crud/repository"
+	"golang-fiber-crud/router"
+	"golang-fiber-crud/service"
 	"log"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber"
 )
 
 func main() {
-	router := fiber.New()
+	fmt.Println("Run service ... ")
+
+	loadConfig, err := config.Loadconfig(",")
+	if err != nil {
+		log.Fatal("could not load environment variables", err)
+	}
+
+	//database
+	db := config.ConnectionDB(&loadConfig)
+	validate := validator.New()
+
+	db.Table("notes").AutoMigrate(&model.Note{})
+
+	//Init Repo
+	noteRepository := repository.NewNoteRepositoryImpl(db)
+
+	//Init service
+	noteService := service.NewNoteServiceImpl(noteRepository, validate)
+
+	//Init Controller
+	noteController := controller.NewNoteController(noteService)
+
+	//Routes
+	routes := router.NewRouter(noteController)
+
 	app := fiber.New()
 
-	app.Mount("/api", router)
-
-	router.Get("/health", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{
-			"status":  "success",
-			"message": "welcome to golang , fiber, and GOrm",
-		})
-	})
-
+	app.Mount("/api", routes)
 	log.Fatal(app.Listen(":8000"))
 }

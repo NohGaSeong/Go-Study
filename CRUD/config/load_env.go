8c5e64379bb7983b9b9380db5b89efff37e59590
yohan <noyohanx@naver.com>
2023-07-24 19:30:08 +0900

    :sparkles: :: LoadConfig 완료

--- CRUD/config/load_env.go
+++ CRUD/config/load_env.go
@@ -1,9 +1,29 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+)
+
 type config struct {
 	DBHost     string `mapstructure:"POSTGRES_HOST"`
 	DBUsername string `mapstructure:"POSTGRES_USER"`
 	DBPassword string `mapstructure:"POSTGRES_PASSWORD"`
 	DBName     string `mapstructure:"POSTGRES_DB"`
 	DBPort     string `mapstructure:"POSTGRES_PORT"`
+}
+
+func Loadconfig(path string) (config config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
 }
